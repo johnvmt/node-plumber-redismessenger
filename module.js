e2c2edd525a556e720fd3af9bd94fcd7e6cfbfeb
johnvmt <johnvmt@gmail.com>
2015-04-13 23:36:25 -0400

    First commit

--- module.js
+++ module.js
@@ -6,23 +6,32 @@ var redis = require("redis");
 
 function RedisMessenger(config) {
 
-	this.client = redis.createClient(config.port, config.host, {});
-	this.client.auth(config.password, function(error, response) {
-		console.log("err", error);
-		console.log("response", response);
-		/* client.set("foo_rand000000000000", "OK");
-
-		// This will return a JavaScript String
-		client.get("foo_rand000000000000", function (err, reply) {
-			console.log(reply.toString()); // Will print `OK`
-		}); */
+	this.pubClient = this.createClient(config, function(error, response) {
+		if(error)
+			console.log("pub create error");
+		else
+			console.log("pub create success");
 	});
 
-
-
+	this.subClient = this.createClient(config, function(error, response) {
+		if(error)
+			console.log("pub create error");
+		else
+			console.log("pub create success");
+	});
 }
+
 
 RedisMessenger.prototype.pub = function(params, callback) {
 
 
+};
+
+RedisMessenger.prototype.createClient = function(config, callback) {
+	var client = redis.createClient(config.port, config.host, {});
+	client.auth(config.password, function(error, response) {
+		if(error)
+			callback(error, null);
+		else
+			callback(null, true);
 };
