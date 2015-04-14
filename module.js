72685444f0743a67ca9ba98bc8ea59db898957ce
johnvmt <johnvmt@gmail.com>
2015-04-13 23:30:49 -0400

    First commit

--- module.js
+++ module.js
@@ -2,8 +2,23 @@ module.exports = function(config) {
 	return new RedisMessenger(config);
 };
 
+var redis = require("redis");
+
 function RedisMessenger(config) {
-	console.log(config);
+
+	this.client = redis.createClient(config.port, config.host, {});
+	this.client.auth(config.password, function(error, response) {
+		console.log("err", error);
+		console.log("response", response);
+		/* client.set("foo_rand000000000000", "OK");
+
+		// This will return a JavaScript String
+		client.get("foo_rand000000000000", function (err, reply) {
+			console.log(reply.toString()); // Will print `OK`
+		}); */
+	});
+
+
 
 }
 
